94b18331daebaa7b87f0acb3a99437920521880a
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 19:22:22 +0300

    Updates

---
+++ precache-manifest.106c7ea44141fc3122edf3825f7d06f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825cff755eb229295e65e8948ea46c6e",
+    "url": "/redux-box/index.html"
+  },
+  {
+    "revision": "1ceb3a811af391dba1af",
+    "url": "/redux-box/static/js/2.59627bce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-box/static/js/2.59627bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaad7b7bf197954139db",
+    "url": "/redux-box/static/js/main.635d7bc6.chunk.js"
+  },
+  {
+    "revision": "58bb5e03f75736348900",
+    "url": "/redux-box/static/js/runtime-main.f51e1d43.js"
+  }
+]);
